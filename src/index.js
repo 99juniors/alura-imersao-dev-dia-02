6d9a7d68082f35673e13c94fc62a96cad7e00239
coderamos <diego.coderamos@gmail.com>
2022-03-08 22:20:22 -0300

    imersão dev - desafios - dia 02

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 function converterRealParaDolar() {
   const valorInserido = parseFloat(document.getElementById('valorRealParaDolar').value);
   const cotacaoDolar = 5;
-  const valorConvertido = valorInserido * cotacaoDolar;
-  const textoValorConvertido = "O valor convertido é: R$ " + valorConvertido;
+  const cotacaoBitcoin = 197142.05;
+  const valorConvertido = (valorInserido / cotacaoDolar).toFixed(2);
+  const valorEmBitcoin = valorInserido / cotacaoBitcoin;
+
+  const textoValorConvertido = "O valor convertido em Dólar é: U$ " + valorConvertido;
+  const textoValorConvertidoEmBitcoin = "O valor convertido em BTC é: BTC " + valorEmBitcoin;
+
   document.getElementById('valorConvertido').innerHTML = textoValorConvertido;
+  document.getElementById('valorConvertidoEmBitcoin').innerHTML = textoValorConvertidoEmBitcoin;
 }
 
 function converterRealParaEuro() {
   const valorInserido = parseFloat(document.getElementById('valorRealParaEuro').value);
-  const cotacaoDolar = 5.52;
-  const valorConvertido = (valorInserido / cotacaoDolar).toFixed(2);
-  const textoValorConvertido = "O valor convertido é: EUR " + valorConvertido;
+  const cotacaoEuro = 5.52;
+  const cotacaoBitcoin = 197142.05;
+  const valorConvertido = (valorInserido / cotacaoEuro).toFixed(2);
+  const valorEmBitcoin = valorInserido / cotacaoBitcoin;
+
+  const textoValorConvertido = "O valor convertido em EUR é: € " + valorConvertido;
+  const textoValorConvertidoEmBitcoin = "O valor convertido em BTC é: BTC " + valorEmBitcoin;
+
   document.getElementById('valorConvertido').innerHTML = textoValorConvertido;
+  document.getElementById('valorConvertidoEmBitcoin').innerHTML = textoValorConvertidoEmBitcoin;
 }
 
