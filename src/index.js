3090f4c88206e12c0c7e0e3cb771f6d3b598bc36
coderamos <diego.coderamos@gmail.com>
2022-03-08 21:46:06 -0300

    imersão dev - desafios - dia 02 - wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-function Converter() {
-  const valorInserido = parseFloat(document.getElementById('valor').value);
+function converterRealParaDolar() {
+  const valorInserido = parseFloat(document.getElementById('valorRealParaDolar').value);
   const cotacaoDolar = 5;
   const valorConvertido = valorInserido * cotacaoDolar;
-
   const textoValorConvertido = "O valor convertido é: R$ " + valorConvertido;
   document.getElementById('valorConvertido').innerHTML = textoValorConvertido;
 }
+
+function converterRealParaEuro() {
+  const valorInserido = parseFloat(document.getElementById('valorRealParaEuro').value);
+  const cotacaoDolar = 5.52;
+  const valorConvertido = (valorInserido / cotacaoDolar).toFixed(2);
+  const textoValorConvertido = "O valor convertido é: EUR " + valorConvertido;
+  document.getElementById('valorConvertido').innerHTML = textoValorConvertido;
+}
+
